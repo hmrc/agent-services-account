b2f7a138520b7787a209fc333de470942a54c629
vaibhvidhami <vaibhavi.dhami@digital.hmrc.gov.uk>
2025-09-12 15:08:23 +0100

    updated library dependancy

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ object AppDependencies {
 
   private val bootstrapVersion = "9.16.0"
   private val hmrcMongoVersion = "2.6.0"
-  private val openHtmlToPdfVersion = "1.0.10"
+  private val openHtmlToPdfVersion = "1.1.31"
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30"    % bootstrapVersion,
@@ -12,13 +12,13 @@ object AppDependencies {
     "uk.gov.hmrc"             %% "agent-mtd-identifiers"        % "3.0.0",
     "uk.gov.hmrc"             %% "crypto-json-play-30"          % "8.2.0",
     "uk.gov.hmrc"             %% "internal-auth-client-play-30" % "4.0.0",
-    "com.openhtmltopdf"       % "openhtmltopdf-pdfbox"          % openHtmlToPdfVersion,
+    "io.github.openhtmltopdf"  % "openhtmltopdf-pdfbox"          % openHtmlToPdfVersion,
   )
 
   val test = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion            % Test,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"    % hmrcMongoVersion            % Test,
-    "org.scalamock"           %% "scalamock"                  % "7.4.0"                     % Test,
+    "org.scalamock"           %% "scalamock"                  % "7.5.0"                     % Test,
   )
 
   val it = Seq.empty
