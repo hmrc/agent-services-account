68fd090cfc4bcbb5141498e50684104def1beb5d
Drew Miley <13115186+drewmiley@users.noreply.github.com>
2026-06-19 14:44:22 +0100

    APB-11584: Defines dummy data required for HipAmendPayloadSpec

--- test/uk/gov/hmrc/agentservicesaccount/models/HipAmendPayloadSpec.scala
+++ test/uk/gov/hmrc/agentservicesaccount/models/HipAmendPayloadSpec.scala
@@ -16,16 +16,86 @@
 
 package uk.gov.hmrc.agentservicesaccount.models
 
-import play.api.libs.json.Json
-//import uk.gov.hmrc.agentservicesaccount.assets.TestConstants.testHipAmendPayload
+import play.api.Logger
+import uk.gov.hmrc.agentmtdidentifiers.model.Utr
+import uk.gov.hmrc.agentservicesaccount.models.AmlsDetails.{EvidenceObjectReference, MembershipNumber, SupervisoryBody}
+import uk.gov.hmrc.agentservicesaccount.models.HipAmendPayload.toHipAmendPayload
 import uk.gov.hmrc.agentservicesaccount.utils.UnitSpec
 
 // TODO: 11584 Add unit tests for HipAmendPayload model here
 class HipAmendPayloadSpec
 extends UnitSpec:
 
-  "HipAmendPayload" should:
-    "true equal true" in:
+  private val logger = Logger("")
+  private val oldRecord = AgentDetailsDesResponse(
+    uniqueTaxReference = Some(Utr("1234567890")),
+    agencyDetails = Some(AgencyDetails(
+      agencyName = Some("Old Agency Name"),
+      agencyEmail = Some("test@email.com"),
+      agencyTelephone = Some("07123456789"),
+      agencyAddress = Some(BusinessAddress(
+        addressLine1 = "Old Address Line 1",
+        addressLine2 = Some("Old Address Line 2"),
+        addressLine3 = Some("Old Address Line 3"),
+        postalCode = Some("AA1 1AA"),
+        countryCode = "GB"
+      ))
+    )),
+    amlsDetails = Some(AmlsDetails(
+      supervisoryBody = SupervisoryBody("SRA"),
+      membershipNumber = MembershipNumber("XAML00000123456"),
+      evidenceObjectReference = Some(EvidenceObjectReference("old-ref-456"))
+    )),
+    suspensionDetails = None,
+    isAnIndividual = None
+  )
+
+  private val agencyDetails = AgencyDetails(
+    agencyName = Some("Test Agency"),
+    agencyEmail = Some("test@example.com"),
+    agencyTelephone = Some("07123456789"),
+    agencyAddress = Some(BusinessAddress(
+      addressLine1 = "1 High Street",
+      addressLine2 = Some("Floor 2"),
+      addressLine3 = Some("Town Centre"),
+      postalCode = Some("TF1 1AA"),
+      countryCode = "GB"
+    ))
+  )
+
+  private val amlsDetails = AmlsDetails(
+    supervisoryBody = SupervisoryBody("SRA"),
+    membershipNumber = MembershipNumber("XAML00000123456"),
+    evidenceObjectReference = Some(EvidenceObjectReference("f28047ef-33f9-482e-a76a-ec4304de7b62"))
+  )
+
+  "toHipAmendPayload" should:
+    "return correct HipAmendPayload when passed AmlsUpdateRequest and useUpdatedHipPutAgentRecord false" in:
+      val agentRecordUpdateRequest: AgentRecordUpdateRequest = AmlsUpdateRequest(amlsDetails)
+      val hipAmendPayload = toHipAmendPayload(agentRecordUpdateRequest)(oldRecord, false)(logger)
+      true shouldBe true
+
+    "return correct HipAmendPayload when passed AgencyDetailsUpdateRequest with name, phone, email only updated and useUpdatedHipPutAgentRecord false" in :
+      true shouldBe true
+
+    "return correct HipAmendPayload when passed AgencyDetailsUpdateRequest with address only updated and useUpdatedHipPutAgentRecord false" in :
+      true shouldBe true
+
+    "return correct HipAmendPayload when passed AgencyDetailsUpdateRequest with all fields updated and useUpdatedHipPutAgentRecord false" in :
+      true shouldBe true
+
+    "return correct HipAmendPayload when passed AmlsUpdateRequest and useUpdatedHipPutAgentRecord true" in :
+      val agentRecordUpdateRequest: AgentRecordUpdateRequest = AmlsUpdateRequest(amlsDetails)
+      val hipAmendPayload = toHipAmendPayload(agentRecordUpdateRequest)(oldRecord, true)(logger)
+      true shouldBe true
+
+    "return correct HipAmendPayload when passed AgencyDetailsUpdateRequest with name, phone, email only updated and useUpdatedHipPutAgentRecord true" in :
+      true shouldBe true
+
+    "return correct HipAmendPayload when passed AgencyDetailsUpdateRequest with address only updated and useUpdatedHipPutAgentRecord true" in :
+      true shouldBe true
+
+    "return correct HipAmendPayload when passed AgencyDetailsUpdateRequest with all fields updated and useUpdatedHipPutAgentRecord true" in :
       true shouldBe true
 //    "serialize to JSON using the implicit mongoFormat" in:
 //      val result = Json.toJson(testHipAmendPayload)
