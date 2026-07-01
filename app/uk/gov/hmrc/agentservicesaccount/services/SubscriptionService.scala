9f30e5d0a73c5e2f46a2271d56f8eec12f2e7959
kaizar12 <10980326+kaizar12@users.noreply.github.com>
2026-07-01 10:46:14 +0100

    APB-11660 restore the condition removed before

--- app/uk/gov/hmrc/agentservicesaccount/services/SubscriptionService.scala
+++ app/uk/gov/hmrc/agentservicesaccount/services/SubscriptionService.scala
@@ -247,7 +247,7 @@ extends Logging:
         )
       case optWorkItem =>
         enrolmentStoreProxyConnector.queryEnrolmentsAllocatedToGroup(groupId).flatMap {
-          case enrolments if enrolments.exists(e => e.service == regime.enrolmentKey) =>
+          case enrolments if enrolments.exists(e => e.service == regime.enrolmentKey && e.state == "Activated") =>
             Future.successful(
               SubscriptionInfo(
                 regime = regime,
