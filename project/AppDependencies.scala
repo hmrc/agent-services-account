9eeff8ecc18f5b4b2d69d80d8e3b0439d98de0a7
raw1697 <romel.w@hotmail.co.uk>
2026-07-01 16:50:39 +0100

    Address dependency vulnerabilities

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,20 +6,21 @@ object AppDependencies {
   private val hmrcMongoVersion     = "2.12.0"
   private val openHtmlToPdfVersion = "1.1.31"
   private val pekkoVer = "1.0.3"
+  private val playVer: String = "play-30"
 
   val compile = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-backend-play-30"         % bootstrapVersion,
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-work-item-repo-play-30" % hmrcMongoVersion,
-    "uk.gov.hmrc"            %% "agent-mtd-identifiers"             % "3.0.0",
-    "uk.gov.hmrc"            %% "crypto-json-play-30"               % "8.4.0",
-    "uk.gov.hmrc"            %% "internal-auth-client-play-30"      % "4.4.0",
+    "uk.gov.hmrc"            %% s"bootstrap-backend-$playVer"         % bootstrapVersion,
+    "uk.gov.hmrc.mongo"      %% s"hmrc-mongo-work-item-repo-$playVer" % hmrcMongoVersion,
+    "uk.gov.hmrc"            %% s"agent-mtd-identifiers"             % "3.0.0",
+    "uk.gov.hmrc"            %% s"crypto-json-$playVer"               % "8.4.0",
+    "uk.gov.hmrc"            %% s"internal-auth-client-$playVer"      % "4.4.0",
     "io.github.openhtmltopdf" % "openhtmltopdf-pdfbox"              % openHtmlToPdfVersion,
-    "io.github.samueleresca" %% "pekko-quartz-scheduler"            % "1.2.0-pekko-1.0.x"
+    "io.github.samueleresca" %% "pekko-quartz-scheduler"            % "1.2.2-pekko-1.0.x"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapVersion % Test,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % hmrcMongoVersion % Test,
+    "uk.gov.hmrc"       %% s"bootstrap-test-$playVer"  % bootstrapVersion % Test,
+    "uk.gov.hmrc.mongo" %% s"hmrc-mongo-test-$playVer" % hmrcMongoVersion % Test,
     "org.apache.pekko" %% "pekko-actor-testkit-typed"% pekkoVer         % Test,
     "org.scalamock"     %% "scalamock"               % "7.5.0"          % Test,
   )
