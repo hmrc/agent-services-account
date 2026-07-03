88d4ec4350e7eb7a31f2f130ae82dc5002037f66
raw1697 <44207483+raw1697@users.noreply.github.com>
2026-07-03 16:36:14 +0100

    Address dependency vulnerabilities (#63)
    * Address dependency vulnerabilities
    
    * Address dependency vulnerabilities

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,28 +1,29 @@
-import sbt._
+import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion     = "10.7.0"
+  private val bootstrapVersion     = "10.8.0"
   private val hmrcMongoVersion     = "2.12.0"
-  private val openHtmlToPdfVersion = "1.1.31"
+  private val openHtmlToPdfVersion = "1.1.37"
   private val pekkoVer = "1.0.3"
+  private val playVer: String = "play-30"
 
-  val compile = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-backend-play-30"         % bootstrapVersion,
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-work-item-repo-play-30" % hmrcMongoVersion,
-    "uk.gov.hmrc"            %% "agent-mtd-identifiers"             % "3.0.0",
-    "uk.gov.hmrc"            %% "crypto-json-play-30"               % "8.4.0",
-    "uk.gov.hmrc"            %% "internal-auth-client-play-30"      % "4.4.0",
+  val compile: Seq[ModuleID] = Seq(
+    "uk.gov.hmrc"            %% s"bootstrap-backend-$playVer"         % bootstrapVersion,
+    "uk.gov.hmrc.mongo"      %% s"hmrc-mongo-work-item-repo-$playVer" % hmrcMongoVersion,
+    "uk.gov.hmrc"            %% s"agent-mtd-identifiers"             % "3.0.0",
+    "uk.gov.hmrc"            %% s"crypto-json-$playVer"               % "8.4.0",
+    "uk.gov.hmrc"            %% s"internal-auth-client-$playVer"      % "4.4.0",
     "io.github.openhtmltopdf" % "openhtmltopdf-pdfbox"              % openHtmlToPdfVersion,
-    "io.github.samueleresca" %% "pekko-quartz-scheduler"            % "1.2.0-pekko-1.0.x"
+    "io.github.samueleresca" %% "pekko-quartz-scheduler"            % "1.2.2-pekko-1.0.x"
   )
 
-  val test = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapVersion % Test,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % hmrcMongoVersion % Test,
+  val test: Seq[ModuleID] = Seq(
+    "uk.gov.hmrc"       %% s"bootstrap-test-$playVer"  % bootstrapVersion % Test,
+    "uk.gov.hmrc.mongo" %% s"hmrc-mongo-test-$playVer" % hmrcMongoVersion % Test,
     "org.apache.pekko" %% "pekko-actor-testkit-typed"% pekkoVer         % Test,
-    "org.scalamock"     %% "scalamock"               % "7.5.0"          % Test,
+    "org.scalamock"     %% "scalamock"               % "7.5.5"          % Test,
   )
 
-  val it = Seq.empty
+  val it: Seq[Nothing] = Seq.empty
 }
