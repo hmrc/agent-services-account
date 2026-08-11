df5e42b7242798f71fd587cdcc87f48af149be13
Drew Miley <13115186+drewmiley@users.noreply.github.com>
2026-08-11 12:25:32 +0100

    APB-11995: Reverts pekko update and address platopsPRbot suggestion for mongo version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,8 +4,8 @@ object AppDependencies {
 
   private val playVer: String = "play-30"
   private val bootstrapVersion: String = "10.8.0"
-  private val hmrcMongoVersion: String = "2.12.0"
-  private val pekkoVer: String = "1.6.0"
+  private val hmrcMongoVersion: String = "2.13.0"
+  private val pekkoVer: String = "1.0.3"
   private val openHtmlToPdfVersion: String = "1.1.37"
 
   val compile: Seq[ModuleID] = Seq(
