bafe2b1459f22349d062f2704730290dfe3e447d
Drew Miley <13115186+drewmiley@users.noreply.github.com>
2026-08-11 12:17:09 +0100

    APB-11995: Updates pekko version for tests

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ object AppDependencies {
   private val playVer: String = "play-30"
   private val bootstrapVersion: String = "10.8.0"
   private val hmrcMongoVersion: String = "2.12.0"
-  private val pekkoVer: String = "1.0.3"
+  private val pekkoVer: String = "1.6.0"
   private val openHtmlToPdfVersion: String = "1.1.37"
 
   val compile: Seq[ModuleID] = Seq(
