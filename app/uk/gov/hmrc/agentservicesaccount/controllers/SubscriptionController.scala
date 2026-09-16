739e743c4accd68f6322829e567772d1e5ec5691
Drew Miley <13115186+drewmiley@users.noreply.github.com>
2026-09-16 12:39:40 +0100

    APB-12021: Renames LegacySubscriptionController to SubscriptionController

--- app/uk/gov/hmrc/agentservicesaccount/controllers/SubscriptionController.scala
+++ app/uk/gov/hmrc/agentservicesaccount/controllers/SubscriptionController.scala
@@ -39,8 +39,8 @@ import uk.gov.hmrc.agentservicesaccount.services.SubscriptionService
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 
 @Singleton
-class LegacySubscriptionController @Inject() (
-  legacySubscriptionService: SubscriptionService,
+class SubscriptionController @Inject()(
+  subscriptionService: SubscriptionService,
   cc: ControllerComponents,
   authActions: AuthActions
 )(using ec: ExecutionContext)
@@ -53,7 +53,7 @@ with RequestAwareLogging:
 
       request.body.asJson.map(_.validate[SubscriptionRequest](using SubscriptionRequest.requestReads(regime))) match {
         case Some(JsSuccess(request: SubscriptionRequest, _)) =>
-          legacySubscriptionService.startSubscriptionProcess(
+          subscriptionService.startSubscriptionProcess(
             arn,
             request,
             regime,
@@ -74,7 +74,7 @@ with RequestAwareLogging:
   def subscriptionInfo(regimes: Seq[LegacyRegime]): Action[AnyContent] = authActions.authorisedWithArnAndGroupId {
     request => (arn, groupId) =>
       given RequestHeader = request
-      legacySubscriptionService.getSubscriptionInfo(
+      subscriptionService.getSubscriptionInfo(
         arn = arn,
         groupId = groupId,
         regimes = regimes
@@ -89,7 +89,7 @@ with RequestAwareLogging:
         logger.error(s"[roboticsCallback] $msg")
         Future.successful(BadRequest(msg))
       else
-        legacySubscriptionService.handleRoboticsCallback(request.body).map {
+        subscriptionService.handleRoboticsCallback(request.body).map {
           case SubscriptionService.CallbackHandling.Handled =>
             val msg = s"Processed callback for requestId: ${request.body.requestId}, success: ${request.body.status}"
             logger.warn(s"[roboticsCallback] $msg")
